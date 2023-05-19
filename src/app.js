ec966b636dfe0f34c8faa8d8ce858b97cb6df127
NadegeRodier <129798006+NadegeRodier@users.noreply.github.com>
2023-05-19 14:55:07 -0500

    Added real data for locations on load

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
+function updateData(city, timeZone) {
+  let cityElement = document.getElementById(city);
+  if (cityElement) {
+    let dateElement = cityElement.querySelector(".date");
+    let timeElement = cityElement.querySelector(".time");
+    dateElement.innerHTML = moment().tz(timeZone).format("MMMM D, YYYY");
+    timeElement.innerHTML = moment()
+      .tz(timeZone)
+      .format("h:mm:ss [<small>]A[</small>]");
+  }
+}
 
+function updateTime() {
+  updateData("los-angeles", "America/Los_Angeles");
+  updateData("sydney", "Australia/Sydney");
+  updateData("tokyo", "Asia/Tokyo");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
